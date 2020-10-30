90429f06ddc4540b24e437da9188c317d6587de3
Chetan Dhangar <chetansdhangar@gmail.com>
2020-10-30 11:15:14 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React,{Component}from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-//pages
+import './App.css';
 
+//component
+import Header from './components/Header';
+//pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import './App.css';
+
 
 class App extends Component {
   render(){
     return (
        <div className="App">
          <Router>
+         <Header/>
+           <div className="container">
+           
            <Switch>
              <Route exact path='/' component={Home}></Route>
              <Route path='/login' component={Login}></Route>
              <Route path='/signup' component={Signup}></Route>
            </Switch>
+           </div>
          </Router>
        </div>  
     );
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import{Link} from 'react-router-dom';
+
+class Header extends Component{
+    render(){
+        return(
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component ={Link} to="/login" >Login</Button>
+                    <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+export default Header;
